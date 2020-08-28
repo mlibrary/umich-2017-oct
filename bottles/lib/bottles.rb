fb070cc51dfaca99ede3f9e1e95f3e84815d98b2
Albert Bertram <bertrama@umich.edu>
2020-08-27 23:15:47 -0400

    The concise approach.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,24 +1,19 @@
 class Bottles
-
   def song
     verses(99, 0)
   end
 
-  def verses(upper, lower)
-    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  def verses(hi, lo)
+    hi.downto(lo).map {|n| verse(n) }.join("\n")
   end
 
-  def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
-    end
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of beer on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+    "#{n > 0 ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+      : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+    " of beer on the wall.\n"
   end
-
 end
